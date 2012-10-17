a71ba829a96647e5b9a209d37a70dd733fc291be
Rémy-Christophe Schermesser <git@schermesser.com>
2012-10-17 23:13:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecDojo1::Application.config.secret_token = 'f71dc34331242777ed485eb20fc18be7f9863370fd0b538deeff996c60c8ad592fd713967e368e28d81a6695bb05a56c95aea50c10a6116eeb80b1d03e0dfb37'
