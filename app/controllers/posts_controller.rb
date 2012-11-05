d1a10d9195181d43b32878e5e6e872af2e3bf9d5
Rémy-Christophe Schermesser <git@schermesser.com>
2012-11-05 18:48:33 +0100

    Adding nested routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,83 +1,95 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.json
-  def index
-    @posts = Post.all
+	# GET /posts
+	# GET /posts.json
+	def index
+		@posts = model_to_query.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @posts }
-    end
-  end
+		respond_to do |format|
+			format.html # index.html.erb
+			format.json { render json: @posts }
+		end
+	end
 
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-    @post = Post.find(params[:id])
+	# GET /posts/1
+	# GET /posts/1.json
+	def show
+		@post = model_to_query.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @post }
-    end
-  end
+		respond_to do |format|
+			format.html # show.html.erb
+			format.json { render json: @post }
+		end
+	end
 
-  # GET /posts/new
-  # GET /posts/new.json
-  def new
-    @post = Post.new
+	# GET /posts/new
+	# GET /posts/new.json
+	def new
+		@post = model_to_query.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @post }
-    end
-  end
+		respond_to do |format|
+			format.html # new.html.erb
+			format.json { render json: @post }
+		end
+	end
 
-  # GET /posts/1/edit
-  def edit
-    @post = Post.find(params[:id])
-  end
+	# GET /posts/1/edit
+	def edit
+		@post = model_to_query.find(params[:id])
+	end
 
-  # POST /posts
-  # POST /posts.json
-  def create
-    @post = Post.new(params[:post])
+	# POST /posts
+	# POST /posts.json
+	def create
+		@post = model_to_query.new(params[:post])
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render json: @post, status: :created, location: @post }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @post.save
+				format.html { redirect_to @post, notice: 'Post was successfully created.' }
+				format.json { render json: @post, status: :created, location: @post }
+			else
+				format.html { render action: "new" }
+				format.json { render json: @post.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PUT /posts/1
-  # PUT /posts/1.json
-  def update
-    @post = Post.find(params[:id])
+	# PUT /posts/1
+	# PUT /posts/1.json
+	def update
+		@post = model_to_query.find(params[:id])
 
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @post.update_attributes(params[:post])
+				format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+				format.json { head :no_content }
+			else
+				format.html { render action: "edit" }
+				format.json { render json: @post.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
+	# DELETE /posts/1
+	# DELETE /posts/1.json
+	def destroy
+		@post = model_to_query.find(params[:id])
+		@post.destroy
 
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
-  end
+		respond_to do |format|
+			format.html { redirect_to posts_url }
+			format.json { head :no_content }
+		end
+	end
+
+	private
+	def model_to_query
+		if params.has_key? :user_id
+			User.find(params[:user_id]).posts
+		elsif params.has_key? :category_id
+			User.find(params[:category_id]).posts
+		else
+			Post
+		end
+	end
+
 end
